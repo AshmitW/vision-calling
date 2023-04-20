28493d531a814614ea03b1ee52df838c3d2afae0
Ashmit <samith.shetty3@gmail.com>
2023-04-20 02:18:26 +0100

    fix: permissions

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import AgoraRTC from 'agora-rtc-sdk-ng';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule, MenuController } from '@ionic/angular';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Router } from '@angular/router';
+import { Camera } from '@capacitor/camera';
+import { Microphone, AudioRecording } from '@mozartec/capacitor-microphone';
 
 @Component({
   selector: 'app-welcome',
@@ -24,6 +26,50 @@ export class WelcomePage implements OnInit {
 
   welcomeCompleted() {
     localStorage.setItem('welcomeCompleted', 'true');
-    this.router.navigateByUrl('/login');
+    this.router.navigateByUrl('/home');
+  }
+
+  async checkVideoPermissions() {
+    try {
+      const checkPermissionsResult = await Camera.checkPermissions();
+      console.log(
+        'checkPermissionsResult: ' + JSON.stringify(checkPermissionsResult)
+      );
+    } catch (error) {
+      console.error('checkPermissions Error: ' + JSON.stringify(error));
+    }
+  }
+
+  async requestVideoPermissions() {
+    try {
+      const requestPermissionsResult = await Camera.requestPermissions();
+      console.log(
+        'requestPermissionsResult: ' + JSON.stringify(requestPermissionsResult)
+      );
+    } catch (error) {
+      console.error('requestPermissions Error: ' + JSON.stringify(error));
+    }
+  }
+
+  async checkMicPermissions() {
+    try {
+      const checkPermissionsResult = await Microphone.checkPermissions();
+      console.log(
+        'checkPermissionsResult: ' + JSON.stringify(checkPermissionsResult)
+      );
+    } catch (error) {
+      console.error('checkPermissions Error: ' + JSON.stringify(error));
+    }
+  }
+
+  async requestMicPermissions() {
+    try {
+      const requestPermissionsResult = await Microphone.requestPermissions();
+      console.log(
+        'requestPermissionsResult: ' + JSON.stringify(requestPermissionsResult)
+      );
+    } catch (error) {
+      console.error('requestPermissions Error: ' + JSON.stringify(error));
+    }
   }
 }
