95a5623e4f13d61a9bddedfef9f81d5ea9ffcb67
Ashmit <samith.shetty3@gmail.com>
2023-04-22 08:10:18 +0100

    feat: added draggable cam and multi ui started

--- src/app/pages/video/video.page.ts
+++ src/app/pages/video/video.page.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RtcService } from 'src/app/services/rtc.service';
 import { Router } from '@angular/router';
+import interact from 'interactjs';
 
 @Component({
   selector: 'app-video',
@@ -40,6 +41,57 @@ export class VideoPage implements OnInit {
         }
       }
     });
+    this.makeFrameDraggable();
+  }
+
+  makeFrameDraggable() {
+    interact('.draggable').draggable({
+      // enable inertial throwing
+      inertia: true,
+      // keep the element within the area of it's parent
+      modifiers: [
+        interact.modifiers.restrictRect({
+          restriction: 'parent',
+          endOnly: true,
+        }),
+      ],
+      // enable autoScroll
+      autoScroll: true,
+
+      listeners: {
+        // call this function on every dragmove event
+        move: dragMoveListener,
+
+        // call this function on every dragend event
+        end(event) {
+          var textEl = event.target.querySelector('p');
+
+          textEl &&
+            (textEl.textContent =
+              'moved a distance of ' +
+              Math.sqrt(
+                (Math.pow(event.pageX - event.x0, 2) +
+                  Math.pow(event.pageY - event.y0, 2)) |
+                  0
+              ).toFixed(2) +
+              'px');
+        },
+      },
+    });
+
+    function dragMoveListener(event) {
+      var target = event.target;
+      // keep the dragged position in the data-x/data-y attributes
+      var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+      var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+      // translate the element
+      target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+
+      // update the posiion attributes
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+    }
   }
 
   async startCall() {
