6175a898df994edfb23121f824564ccc678362a1
Ashmit <samith.shetty3@gmail.com>
2023-04-22 12:55:58 +0100

    fix: auth guard fix

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -6,7 +6,7 @@ export class AuthGuardService implements CanActivate {
   constructor(public userService: UserService, public router: Router) {}
   canActivate(): boolean {
     if (!this.userService.isAuthenticated()) {
-      this.router.navigate(['home']);
+      this.router.navigate(['login']);
       return false;
     }
     return true;
